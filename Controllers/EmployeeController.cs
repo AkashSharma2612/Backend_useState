eda64fa1aa060617ffb2bdf321f075fbeba7661c
Akash <AkashSharma4373@gmail.com>
2023-01-16 10:32:26 +0530

    backend

---
+++ Controllers/EmployeeController.cs
@@ -0,0 +1,58 @@
+﻿using Employee_Department.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Employee_Department.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeeController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+        public EmployeeController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        [HttpGet]
+        public IActionResult GetEmployees()
+       {
+            var employeeList = _context.Employees.ToList();
+            return Ok(employeeList);
+        }
+        [HttpPost]
+        public async Task<IActionResult> SaveEmployee([FromBody] List<Employee> employee)
+        {
+           
+                foreach (Employee emp in employee)
+            {
+                await _context.Employees.AddAsync(emp);
+                _context.SaveChanges();
+
+            }
+            
+            return Ok(new { message = "data Saved" });
+        }
+
+
+        [HttpPut]
+        public IActionResult UpdateEmployee([FromBody]Employee employee)
+        {
+            _context.Employees.Update(employee);
+            _context.SaveChanges();
+            return Ok(new { message = "data updated" });
+        }
+        [HttpDelete("{id}")]
+        public IActionResult DeleteEmployee(int id)
+        {
+           
+            var employeeInDb = _context.Employees.Find(id);
+            _context.Employees.Remove(employeeInDb);
+            _context.SaveChanges();
+            return Ok(new { message = "data deleted" });
+        }
+    }
+}
---
+++ Migrations/20230111070651_initload.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Employee_Department.Migrations
+{
+    public partial class initload : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Employees",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Department = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Status = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Employees", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Employees");
+        }
+    }
+}
